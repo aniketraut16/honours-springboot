ea2ba2c55bbab3f46db6b7a614ebf86d253b092b
aniketraut16 <rautan_1@rknec.edu>
2025-03-07 10:38:35 +0530

    Initial COmmit

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/")
+    List<String> getAllUsers(){
+        return  List.of("A","B");
+
+    }
+
+}
